af63f06f86e3b95689c54b214be946feada5a9d9
Geert Lorang <geert@lorang.be>
2022-04-07 21:55:32 +0200

    Initial commit

---
+++ rest.php
@@ -0,0 +1,65 @@
+<?php
+
+header("Content-Type: application/json; charset=utf-8");
+
+// Get parameters
+$action = (isset($_GET["action"])) ? preg_replace("/[^-a-zA-Z0-9_]/", "", $_GET["action"]) : "";
+$param = (isset($_GET["param"])) ? preg_replace("/[^-a-zA-Z0-9_]/", "", $_GET["param"]) : "";
+$secret = (isset($_GET["secret"])) ? preg_replace("/[^-a-zA-Z0-9_]/", "", $_GET["secret"]) : "";
+
+// SQLite database
+$sqlitedb = "../word2vec/word2vec.db";
+
+if($action == "model2") {
+	$db = new SQLite3($sqlitedb);
+	$sql = $db->prepare("SELECT vec, percentile FROM word2vec LEFT OUTER JOIN nearby ON nearby.word = :secret AND nearby.neighbor = :word WHERE word2vec.word = :word");
+	$sql->bindValue(":word", $param, SQLITE3_TEXT);
+	$sql->bindValue(":secret", $secret, SQLITE3_TEXT);
+	$result = $sql->execute()->fetchArray();
+
+  $output["vec"] = array();
+
+  $vec = $result[0];
+  for($i=0;$i<strlen($vec);$i+=2) {
+    // expand_bfloat
+    $bytes = "\00\00" . $vec[$i] . $vec[$i+1];
+    $unpacked = unpack("f*", $bytes);
+    array_push($output["vec"], $unpacked[1]);
+  }
+
+  if(isset($result["percentile"])) {
+    $output["percentile"] = intval($result["percentile"]);
+  }
+
+  if(count($output["vec"]) > 0) {
+    echo(json_encode($output));
+  }
+}
+
+if($action == "similarity") {
+	$db = new SQLite3($sqlitedb);
+	$sql = $db->prepare("SELECT top, top10, rest FROM similarity_range WHERE word = :word");
+	$sql->bindValue(":word", $param, SQLITE3_TEXT);
+	$result = $sql->execute()->fetchArray();
+
+  $output["top"] = $result["top"];
+  $output["top10"] = $result["top10"];
+  $output["rest"] = $result["rest"];
+  
+	echo(json_encode($output));
+}
+
+if($action == "nearby") {
+	$db = new SQLite3($sqlitedb);
+	$sql = $db->prepare("SELECT neighbor FROM nearby WHERE word = :word ORDER BY percentile DESC LIMIT 10 OFFSET 1");
+	$sql->bindValue(":word", $param, SQLITE3_TEXT);
+  $result = $sql->execute();
+
+  $output = array();
+
+	while($row = $result->fetchArray()) {
+    array_push($output, $row[0]);
+  }
+   
+	echo(json_encode($output));
+}
---
+++ top1k.php
@@ -0,0 +1,55 @@
+<?php
+
+// Get parameters
+$param = (isset($_GET["param"])) ? preg_replace("/[^-a-zA-Z0-9_]/", "", $_GET["param"]) : "";
+
+$secret = base64_decode($param);
+
+// SQLite database
+$sqlitedb = "../word2vec/word2vec.db";
+
+$db = new SQLite3($sqlitedb);
+$sql = $db->prepare("SELECT neighbor, percentile, similarity FROM nearby WHERE word = :word ORDER BY percentile DESC LIMIT 1000 OFFSET 1");
+$sql->bindValue(":word", $secret, SQLITE3_TEXT);
+$result = $sql->execute();
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <script src="/assets/js/secretWords.js"></script>
+    <script src="/assets/js/nearest1k.js"></script>
+    <title>Semantle: nearest words</title>
+  </head>
+  <body>
+    <div id="warning" style="display:none; ">
+      <div style="border:thin solid black; background-color: #ffdddd">
+        Opgepast: Deze pagina verraadt de opgave van vandaag. Klik op deze waarschuwing om de inhoud toch te zien.
+      </div>
+    </div>
+    <div id="nearest" style="display:none;">
+      Volgende woorden zijn het meest gerelateerd aan <b id="word"><?php echo($secret); ?></b>:
+      <br /><br />
+      <table >
+        <tr>
+          <th>Woord</th>
+          <th>Score</th>
+          <th>&permil;</th>
+        </tr>
+        <?php while($row = $result->fetchArray()) { ?>
+        <tr>
+          <td> <?php echo($row["neighbor"]); ?> </td>
+          <td style="width: 100px; text-align:center;"> <?php echo(sprintf("%0.2f", $row["similarity"] * 100)); ?> </td>
+          <td style="width: 100px; text-align:center;"> <?php echo(intval($row["percentile"])); ?> </td>
+        </tr>
+        <?php } ?>
+      </table>
+    </div>
+
+    <br/>
+    <a href="/">Terug naar Semantle</a>
+  </body>
+</html>
